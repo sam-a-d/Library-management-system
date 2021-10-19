951e7e880f0ff2692d7404129f1354574c70535f
Samad-sust <masamad.sust@gmail.com>
2021-10-19 22:07:00 +0600

    1. password reset and forgot functionality added Not testted due to not having a server

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_view
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,13 @@ urlpatterns = [
     path('logout/', views.logoutUser, name='logout'),
     path('profile/', views.user_profile, name='user-profile'),
     path('profile/edit/', views.user_profile_edit, name='user_profile_edit'),
+
+    path('reset_password/', auth_view.PasswordResetView.as_view(template_name="auth/pass_reset/password_reset.html"), name='reset_password'),
+    
+    path('reset_password_sent/', auth_view.PasswordResetDoneView.as_view(template_name="auth/pass_reset/password_reset_sent.html"), name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name="auth/pass_reset/password_reset_form.html"), name='password_reset_confirm'),
+    
+    path('reset_password_complete/', auth_view.PasswordResetCompleteView.as_view(template_name="auth/pass_reset/password_reset_done.html"), name='password_reset_complete'),
+
 ]
