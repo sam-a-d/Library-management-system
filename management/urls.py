a702197cc28bf4f7a4ba6ab487060dcc814d1df0
Samad-sust <masamad.sust@gmail.com>
2021-10-22 09:47:46 +0600

    1. Management Views restricted by using decorators 2. Managements templates seperated by folder

--- management/urls.py
+++ management/urls.py
@@ -3,5 +3,8 @@ from . import views
 from django_email_verification import urls as email_urls
 
 urlpatterns = [
-    path('all_books/', views.BookListView.as_view(template_name="admin/book_list.html"), name='all_books'),
+    path('all_books/', views.BookListView.as_view(template_name="management/book_list.html"), name='all_books'),
+    path('book/add/', views.BookCreateView.as_view(template_name="management/mgt_generic.html"), name='book-add'),
+    path('book/update/<int:pk>', views.BookUpdateView.as_view(template_name="management/mgt_generic.html"), name='book-update'),
+    path('author/update/<int:pk>', views.AuthorUpdateView.as_view(template_name="management/mgt_generic.html"), name='author-update'),
 ]
--- management/views.py
+++ management/views.py
@@ -1,14 +1,42 @@
 from django.shortcuts import render
+from django.urls import reverse
 
-from root.views import Book
+from root.models import Book, Author
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required, user_passes_test
 
+from django.contrib.auth.models import Group
 # import generic views
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
+from django.utils.decorators import method_decorator
 
-
+from  user.decorators import admin_only_access
 # Create your views here.
 
+@method_decorator(admin_only_access, name='dispatch')
 class BookListView(ListView):
     model = Book
     context_object_name = 'books'
+
+@method_decorator(admin_only_access, name='dispatch')
+class BookCreateView(CreateView):
+    model = Book
+    fields = '__all__'
+    def get_success_url(self):
+        return reverse('all_books')
+        
+@method_decorator(admin_only_access, name='dispatch')
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = '__all__'
+    def get_success_url(self):
+        return reverse('all_books')
+
+@method_decorator(admin_only_access, name='dispatch')
+class AuthorUpdateView(UpdateView):
+    model = Author
+    fields = '__all__'
+    def get_success_url(self):
+        return reverse('all_books')
